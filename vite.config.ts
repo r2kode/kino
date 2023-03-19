23348f11ca2492b782a9cddf8c22886c5ab9d889
r2k <r2kodepl@gmailcom>
2023-03-19 20:45:38 +0100

    chore: setup path alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vite';
+import path from 'path';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   test: {
     globals: true,
     environment: 'jsdom',
