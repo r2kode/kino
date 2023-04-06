9a907bde3ac1f03a3c1faff9fd972df104e9a15b
r2k <r2kodepl@gmailcom>
2023-04-06 18:47:10 +0200

    refactor: layout tweaks

--- src/features/search/view/SearchInput.tsx
+++ src/features/search/view/SearchInput.tsx
@@ -1,5 +1,5 @@
 import { useState, SyntheticEvent } from 'react';
-import { Box, InputBase, IconButton } from '@mui/material';
+import { Box, Paper, InputBase, IconButton } from '@mui/material';
 import SearchIcon from '@mui/icons-material/Search';
 
 type SearchInputProps = {
@@ -15,30 +15,32 @@ export function SearchInput({ handleSearch }: SearchInputProps) {
   };
 
   return (
-    <Box
-      component="form"
-      sx={{
-        bgcolor: '#fff',
-        p: '2px 4px',
-        mb: 4,
-        display: 'flex',
-        alignItems: 'center',
-      }}
-    >
-      <InputBase
-        sx={{ ml: 1, flex: 1 }}
-        placeholder="Search Movie Title"
-        inputProps={{ 'aria-label': 'search movie title' }}
-        onChange={(e) => setSearchQuery(e.target.value)}
-      />
-      <IconButton
-        type="submit"
-        sx={{ p: '10px' }}
-        aria-label="search"
-        onClick={handleSearchSubmit}
+    <Box>
+      <Paper
+        component="form"
+        sx={{
+          bgcolor: '#fff',
+          p: '2px 4px',
+          mb: 4,
+          display: 'flex',
+          alignItems: 'center',
+        }}
       >
-        <SearchIcon />
-      </IconButton>
+        <InputBase
+          sx={{ ml: 1, flex: 1 }}
+          placeholder="Search Movie Title"
+          inputProps={{ 'aria-label': 'search movie title' }}
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
+        <IconButton
+          type="submit"
+          sx={{ p: '10px' }}
+          aria-label="search"
+          onClick={handleSearchSubmit}
+        >
+          <SearchIcon />
+        </IconButton>
+      </Paper>
     </Box>
   );
 }
--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,18 +1,15 @@
 import { Outlet } from 'react-router-dom';
 import Container from '@mui/material/Container';
 import Box from '@mui/material/Box';
-// import CssBaseline from '@mui/material/CssBaseline';
+import CssBaseline from '@mui/material/CssBaseline';
 import { Header } from '@/features/header';
 
 export function Layout() {
   return (
     <>
-      {/* <CssBaseline /> */}
+      <CssBaseline />
       <Header />
-      <Container
-        maxWidth="sm"
-        sx={{ bgcolor: '#242424', color: '#fff', pt: 4 }}
-      >
+      <Container maxWidth="sm" sx={{ pt: 4 }}>
         <Box sx={{ height: '100vh' }}>
           <Outlet />
         </Box>
